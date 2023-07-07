c243ee5fa14c9fe5d24ad44eba3c008f5149560b
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-07-07 08:56:13 +0200

    refactored canActivate guards

--- frontend/src/app/services/guards/admin-auth.guard.ts
+++ frontend/src/app/services/guards/admin-auth.guard.ts
@@ -1,27 +1,43 @@
-import { Injectable } from "@angular/core";
+import { Injectable, inject } from "@angular/core";
 import { AuthService } from "../auth.service";
 import { Router } from "@angular/router";
 import { NgToastService } from "ng-angular-popup";
 
-@Injectable({
-  providedIn : 'root'
-})
-
-export class AdminAuthGuard {
-  constructor (
-    private auth :AuthService,
-    private router : Router,
-    private toast :NgToastService
-    ){}
-
-    canActivate (): boolean{
-      if (!this.auth.isAdmin()){
-        this.router.navigate ([''])
-        this.toast.warning ({detail : 'warning' ,summary :'you can not access to this page.',
-        duration : 3000
-      })
+export const AdminAuthGuard = () =>{
+  const auth = inject(AuthService)
+  const router = inject (Router)
+  const toast = inject(NgToastService)
+      if(!auth.isAdmin()){
+        router.navigate([''])
+        toast.warning ({detail : 'warning' ,summary :'you can not access to this page.',
+                 duration : 3000
+              })
       }
-        this.auth.isAdmin()
-        return true
-      }
-    }
+      auth.isAdmin()
+      return true
+}
+
+// @Injectable({
+//   providedIn : 'root'
+// })
+
+// export class AdminAuthGuard {
+//   constructor (
+//     private auth :AuthService,
+//     private router : Router,
+//     private toast :NgToastService
+//     ){}
+
+//     canActivate (): boolean{
+//       if (!this.auth.isAdmin()){
+//         this.router.navigate ([''])
+//         this.toast.warning ({detail : 'warning' ,summary :'you can not access to this page.',
+//         duration : 3000
+//       })
+//       }
+//         this.auth.isAdmin()
+//         return true
+//       }
+//     }
+
+  
--- frontend/src/app/services/guards/auth.guard.ts
+++ frontend/src/app/services/guards/auth.guard.ts
@@ -1,26 +1,45 @@
-import { Injectable } from "@angular/core";
+import { Injectable, inject } from "@angular/core";
 import { Router } from "@angular/router";
 import { NgToastService } from "ng-angular-popup";
 import { AuthService } from "../auth.service";
 
-@Injectable({
-  providedIn : 'root'
-})
 
-export class AuthGuard {
-  constructor (
-    private auth :AuthService,
-    private router : Router,
-    private toast :NgToastService
-    ){}
 
-    canActivate (): boolean{
-      if (! this.auth.isLoggedin()){
-        this.router.navigate (['login'])
-        this.toast.warning ({detail : 'warning' ,summary :'you should login first.'})
-      }
-        this.auth.isLoggedin()
-        return true
-      }
-    }
+export const AuthGuard = ()=>{
+  const auth = inject(AuthService)
+  const router = inject (Router)
+  const toast = inject(NgToastService)
 
+  if (!auth.isLoggedin()){
+    router.navigate (['login'])
+    toast.warning ({detail : 'warning' ,
+    summary :'you should login first.',
+    duration :3000 
+  })
+  }
+  auth.isLoggedin()
+  return true;
+}
+// @Injectable({
+//   providedIn : 'root'
+// })
+
+// export class AuthGuard {
+//   constructor (
+//     private auth :AuthService,
+//     private router : Router,
+//     private toast :NgToastService
+//     ){}
+
+//     canActivate (): boolean{
+//       if (! this.auth.isLoggedin()){
+//         this.router.navigate (['login'])
+//         this.toast.warning ({detail : 'warning' ,summary :'you should login first.'})
+//       }
+//         this.auth.isLoggedin()
+//         return true
+//       }
+//     }
+
+
+   
