41a6b0da4aeb6f32e96868cc3db62ce7cb1df8e9
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-07-14 13:39:47 +0200

    cancled extra codes.

--- frontend/src/app/core/services/products.service.ts
+++ frontend/src/app/core/services/products.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable,  map } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import IProduct from 'src/app/shared/models/product.model';
 
 @Injectable({
