6965d2438c7f3ea8085f3aaaa8d51679386fc86c
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-07-17 20:16:07 +0200

    added NgbActoveModal service.

--- frontend/src/app/shared/components/cart/cart.component.ts
+++ frontend/src/app/shared/components/cart/cart.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core'
 import IProduct from '../../models/product.model';
 import { CartService } from 'src/app/core/services/cart.service';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-cart',
@@ -10,30 +14,61 @@ import { CartService } from 'src/app/core/services/cart.service';
 export class CartComponent implements OnInit {
 
   products !:IProduct[];
-  totalPrice =0;
+  totalPrice :number=0;
+  total =0;
+  addedToWishList =false;
   
 
- constructor (private cart :CartService){}
+ constructor (
+  private cart :CartService,
+  private activeModal :NgbActiveModal,
+  private router:Router
+  ){}
    
  ngOnInit(): void {
-   this.cart.getProducts().subscribe(
-    res =>{
-      this.products =res
-      this.products.forEach(product =>{
-        Object.assign(product,{totalPrice : 0})
-      })
-      this.totalPrice=this.cart.getTotalPrice()
-     
-    }
-   )
+  this.getTotal()
+ }
+ 
+ getTotal(){
+  this.cart.getProducts().subscribe(res =>{
+    this.products=res;
+  this.total =  this.products.reduce((acc,product)=>{
+     return acc + (product.price*product.quantity)
+    },0)
+  })
  }
 
+
  removeProduct(product:IProduct){
-  this.cart.removeCartItem(product) 
+  this.cart.removeItem(product) 
+ this.getTotal()
  }
 
  removeAllProducts (){
   this.cart.removeAllCartItem()
+ }
+
+ close(){
+ this.activeModal.close()
+ }
+
+ addItems(){
+  this.activeModal.close() 
+  this.router.navigate(['/products'])
+ }
+
+ addItemToWishList(product :IProduct){
+this.addedToWishList =true;
+this.cart.removeItem(product)
+this.router.navigate(['wishlist'])
+ }
+
+ removeItemFromWishList(){
+this.addedToWishList =false;
+ }
+
+ changeQuantity(){
+ this.getTotal()
  }
 
  }
