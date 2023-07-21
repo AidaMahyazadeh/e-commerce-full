d355bdfdb0ed513c347992128566c57ef2cca83c
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-07-21 06:51:37 +0200

    refactored codes.

--- frontend/src/app/shared/components/home/home.component.ts
+++ frontend/src/app/shared/components/home/home.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+
+
+import { Component } from '@angular/core';
 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+ 
 })
 export class HomeComponent {
 //   categories$ ! : Observable<ICategory []>;
