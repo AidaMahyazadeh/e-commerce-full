5bb29f7a3a62fb1b5fb4dd04ad622ec994fc42e8
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-07-28 11:44:35 +0200

    canceled extra codes.

--- frontend/src/app/shared/components/home/home.component.ts
+++ frontend/src/app/shared/components/home/home.component.ts
@@ -6,14 +6,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
- 
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-//   categories$ ! : Observable<ICategory []>;
-//   constructor (private category : ProductsService){}
-// ngOnInit(): void{
-//   this.categories$ =this.category.getCategories()
-// //  this.category.getCategories().subscribe(res =>this.categories=res)
-//  }
+
 }
