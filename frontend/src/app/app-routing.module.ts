73dbcb940331b75ab240ee5c611af24e0fce6021
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-07-28 09:13:31 +0200

    put authGuard for some components.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,19 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './shared/components/login/login.component';
 import { HomeComponent } from './shared/components/home/home.component';
 import { SignupComponent } from './shared/components/signup/signup.component';
-import { CartComponent } from './shared/components/cart/cart.component';
-import { AuthGuard } from './core/services/guards/cart-auth.guard';
 import { AdminAuthGuard } from './core/services/guards/admin-auth.guard';
 import { WishlistComponent } from './shared/components/wishlist/wishlist.component';
+import { AuthGuard } from './core/services/guards/auth.guard';
 
 
 const routes: Routes = [
   {path : '',pathMatch : 'full',component :LoginComponent  },
-  {path : 'home',component:HomeComponent},
+  {path : 'home',component:HomeComponent ,canActivate :[AuthGuard]},
   {path : 'login', component : LoginComponent},
   {path : 'signup', component : SignupComponent},
-  {path :'cart' , component : CartComponent},
-  {path :'wishlist', component :WishlistComponent},
+  // {path :'cart' , component : CartComponent,canActivate :[AuthGuard]},
+  {path :'wishlist', component :WishlistComponent,canActivate :[AuthGuard]},
   {path :'products',loadChildren : () =>import('./modules/products/products.module').then (m =>m.ProductsModule)},
   { path: 'admin',canActivate : [AdminAuthGuard], loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule) }   
 ];
