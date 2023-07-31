7bd1ea65593c0c53c30ff474441979f7e6278ace
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-07-31 15:30:04 +0200

    created payment component.

--- frontend/src/app/shared/components/payment/payment.component.ts
+++ frontend/src/app/shared/components/payment/payment.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  IPayPalConfig,
+  ICreateOrderRequest 
+} from 'ngx-paypal';
 import { AuthStorageService } from 'src/app/core/services/auth-storage.service';
 
 @Component({
@@ -7,12 +11,81 @@ import { AuthStorageService } from 'src/app/core/services/auth-storage.service';
   styleUrls: ['./payment.component.css']
 })
 export class PaymentComponent implements OnInit {
- cartTotal :number =0;
+payPalConfig ?:IPayPalConfig;
+showSuccess !:boolean;
+showError !:boolean;
+showCancel !:boolean;
+cartTotal :number =0;
+
   constructor(
     private authStorage :AuthStorageService
   ){}
 
   ngOnInit() {
-   this.cartTotal = this.authStorage.getTotal()
+   this.cartTotal = this.authStorage.getTotal();
+   this.initConfig();
   }
+
+  private initConfig(): void {
+    this.payPalConfig = {
+        currency: 'EUR',
+        clientId: 'sb',
+        createOrderOnClient: (data) => < ICreateOrderRequest > {
+            intent: 'CAPTURE',
+            purchase_units: [{
+                amount: {
+                    currency_code: 'EUR',
+                    value: `${this.cartTotal}`,
+                    breakdown: {
+                        item_total: {
+                            currency_code: 'EUR',
+                            value: `${this.cartTotal}`
+                        }
+                    }
+                },
+                items: [{
+                    name: 'Enterprise Subscription',
+                    quantity: '1',
+                    category: 'DIGITAL_GOODS',
+                    unit_amount: {
+                        currency_code: 'EUR',
+                        value: `${this.cartTotal}`,
+                    },
+                }]
+            }]
+        },
+        advanced: {
+            commit: 'true'
+        },
+        style: {
+            label: 'paypal',
+            layout: 'vertical'
+        },
+        onApprove: (data, actions) => {
+            console.log('onApprove - transaction was approved, but not authorized', data, actions);
+            actions.order.get().then((details: any) => {
+                console.log('onApprove - you can get full order details inside onApprove: ', details);
+            });
+
+        },
+        onClientAuthorization: (data) => {
+            console.log('onClientAuthorization - you should probably inform your server about completed transaction at this point', data);
+            this.showSuccess = true;
+        },
+        onCancel: (data, actions) => {
+            console.log('OnCancel', data, actions);
+            this.showCancel = true;
+
+        },
+        onError: err => {
+            console.log('OnError', err);
+            this.showError = true;
+        },
+        onClick: (data, actions) => {
+            console.log('onClick', data, actions);
+            // this.resetStatus();
+        }
+    };
 }
+}
+
