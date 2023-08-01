a4d1dc6179b41694c6ba6611f0f7c9c0a9f2d97a
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-08-01 09:35:01 +0200

    adding number of favoraite items.

--- frontend/src/app/components/nav/nav.component.ts
+++ frontend/src/app/components/nav/nav.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
 import { AuthStorageService } from 'src/app/core/services/auth-storage.service';
 import { CartService } from 'src/app/core/services/cart.service';
+import { WishlistService } from 'src/app/core/services/wishlist.service';
 import { CartComponent } from 'src/app/shared/components/cart/cart.component';
 
 
@@ -15,6 +16,7 @@ import { CartComponent } from 'src/app/shared/components/cart/cart.component';
 export class NavComponent implements OnInit{
   crownIcon = './assets/icons/crown.svg';
   cartItems !:number;
+  favoraiteItems !:number;
   
    
   constructor (
@@ -22,11 +24,15 @@ export class NavComponent implements OnInit{
     private router :Router,
     private cart :CartService,
     private modalService :NgbModal,
+    private wishlistService :WishlistService
     ){}
 
     ngOnInit(){
       this.cart.getProducts().subscribe(
         res=>this.cartItems = res.length
+      )
+      this.wishlistService.getProducts().subscribe(
+        res => this.favoraiteItems = res.length
       )
     } 
 
