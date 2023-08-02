f3bc451fe597fb26e5d6e8026da453dab8ac19af
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-08-02 11:49:16 +0200

    canceled extra spaces.

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,9 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptor } from './services/auth.interceptor';
-
-
 
 
 @NgModule({
