0d710372d2ae2304582827f845e1ca0a91b814cf
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-08-31 14:58:33 +0200

    put route for admin module.

--- frontend/src/app/modules/admin/admin-routing.module.ts
+++ frontend/src/app/modules/admin/admin-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
-import { UsersListComponent } from './users-list/users-list.component';
+import { AdminCategoryComponent } from './components/admin-category/admin-category.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
+import { AdminProductsComponent } from './components/admin-products/admin-products.component';
 
 const routes: Routes = [{ path: '', component: AdminComponent, children : [
-  { path : 'users-list' ,component :UsersListComponent}
+  { path : 'users-list' ,component :UsersListComponent},
+  {path :'category',component : AdminCategoryComponent},
+  {path :'product', component :AdminProductsComponent}
 
 ]}
 
