632128634e913c0605b554da3f3b99d1424a6ed7
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-08-31 14:51:30 +0200

    renamed favoraiteItemsLength property.

--- frontend/src/app/components/nav/nav.component.ts
+++ frontend/src/app/components/nav/nav.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgbModal} from '@ng-bootstrap/ng-bootstrap';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, concatMap } from 'rxjs';
 import { AuthStorageService } from 'src/app/core/services/auth-storage.service';
 import { CartService } from 'src/app/core/services/cart.service';
 import { WishlistService } from 'src/app/core/services/wishlist.service';
@@ -17,7 +17,7 @@ import { CartComponent } from 'src/app/shared/components/cart/cart.component';
 export class NavComponent implements OnInit{
   crownIcon = './assets/icons/crown.svg';
   cartItems !:number;
-  favoraiteItems !:number;
+  favoraiteItemsLength !:number;
   favoraiteItemsSubject$ = new BehaviorSubject <number>(0)
   
    
@@ -35,19 +35,32 @@ export class NavComponent implements OnInit{
       this.cart.getProducts().subscribe(
         res=>{
           this.cartItems = res.length
-          this.getLengthOfFavoraiteItems()
+           this.getLengthOfFavoraiteItems()
         }
       )
+      
+     
     } 
 
    getLengthOfFavoraiteItems(){
-     this.wishlistService.getProducts().subscribe(
-      // this.wishlistService.getWishListProduct().subscribe(
-      res =>{
-        this.favoraiteItems= res.length
-        this.favoraiteItemsSubject$.next(this.favoraiteItems)
+    this.wishlistService.wishListSubject$.subscribe(
+      res => {
+        this.favoraiteItemsLength = res.length
       }
     )
+    // this.wishlistService.wishListSubject$.subscribe(
+    //   res  =>{
+    //     this.favoraiteItemsLength = res.length
+    //     this.favoraiteItemsSubject$.next(this.favoraiteItemsLength)
+    //   }
+    // )
+     //this.wishlistService.getProducts().subscribe(
+    //    this.wishlistService.getWishListProduct().subscribe(
+    //   res =>{
+    //      this.favoraiteItemsLength= res.length
+    //      this.favoraiteItemsSubject$.next(this.favoraiteItemsLength)
+    //   }
+    // )
    }
    
     isLoggedin (){
