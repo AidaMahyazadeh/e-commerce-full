746a175da7a275b28acb77daf5c259a4ad852626
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-08 15:46:25 +0200

    updated.

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,1 +1,4 @@
-export const environment = {};
+export const environment = {
+    // production : false ,
+    // client_ID : 'AdHCedh2wU5YvAsaBMUQFXSrqh3DhPq5Ab4CeOh9GYeUrzuVSjpTtWMZh-cBSK__Nffo1_aQZxhAB5Yx'
+};
